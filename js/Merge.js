670d50ef17a6caab27cc3eca0c815d58828c7656
vagrant <keerthi-351553>
2017-01-25 07:15:03 +0000

    initial commit

---
+++ js/Merge.js
@@ -0,0 +1,29 @@
+var fs=require('fs');
+function ReadAppend(file,appendFile){
+
+ //Reading the append file
+fs.readFile(appendFile,function(err,data){
+  if(err){
+    console.log('File was not Read');
+    throw err;
+  }
+  console.log('File was Read');
+
+  fs.appendFile(file,data,function(err){
+    console.log('File')
+    if(err){
+      console.log('File was not append ');
+      throw err;
+    }
+    console.log('The "data to append" was appended to a file');
+    });
+  });
+}
+
+file='../csv/India2011_Merge_csv.csv';
+ // appendFile='../csv/India2011.csv';
+ // file = ReadAppend(file,appendFile);
+ //appendFile='../csv/IndiaSC2011.csv';
+  //file = ReadAppend(file,appendFile);
+appendFile='../csv/IndiaST2011.csv';
+ReadAppend(file,appendFile);

0d43bdc766402e966c6543d0d75dd9c9fd12107d
vagrant <keerthi-351553>
2017-01-30 04:12:34 +0000

    Proper submit

--- js/Merge.js
+++ js/Merge.js
@@ -1,29 +1,29 @@
-var fs=require('fs');
-function ReadAppend(file,appendFile){
+var fs = require('fs');
+function ReadAppend(file, appendFile) {
 
- //Reading the append file
-fs.readFile(appendFile,function(err,data){
-  if(err){
-    console.log('File was not Read');
-    throw err;
-  }
-  console.log('File was Read');
+    //Reading the append file
+    fs.readFile(appendFile, function(err, data) {
+        if (err) {
+            console.log('File was not Read');
+            throw err;
+        }
+        console.log('File was Read');
 
-  fs.appendFile(file,data,function(err){
-    console.log('File')
-    if(err){
-      console.log('File was not append ');
-      throw err;
-    }
-    console.log('The "data to append" was appended to a file');
+        fs.appendFile(file, data, function(err) {
+            console.log('File')
+            if (err) {
+                console.log('File was not append ');
+                throw err;
+            }
+            console.log('The "data to append" was appended to a file');
+        });
     });
-  });
 }
 
-file='../csv/India2011_Merge_csv.csv';
- // appendFile='../csv/India2011.csv';
- // file = ReadAppend(file,appendFile);
- //appendFile='../csv/IndiaSC2011.csv';
-  //file = ReadAppend(file,appendFile);
-appendFile='../csv/IndiaST2011.csv';
-ReadAppend(file,appendFile);
+file = '../csv/India2011_Merge_csv.csv';
+appendFile = '../inputdata/India2011.csv';
+file = ReadAppend(file, appendFile);
+appendFile = '../inputdata/IndiaSC2011.csv';
+file = ReadAppend(file, appendFile);
+appendFile = '../inputdata/IndiaST2011.csv';
+ReadAppend(file, appendFile);
